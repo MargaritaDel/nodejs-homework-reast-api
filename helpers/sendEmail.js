ff7a0993b69644aad66dd47309ac758c7c3fdadd
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-07-22 21:43:09 +0300

    done

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,22 +2,22 @@ const nodemailer = require('nodemailer');
 
 require('dotenv').config();
 
-const { META_EMAIL, META_PASSWORD } = process.env;
+const { USER_EMAIL, USER_PASSWORD } = process.env;
 
 const config = {
   host: 'smtp.meta.ua',
   port: 465,
   secure: true,
   auth: {
-    user: META_EMAIL,
-    pass: META_PASSWORD,
+    user: USER_EMAIL,
+    pass: USER_PASSWORD,
   },
 };
 
 const transporter = nodemailer.createTransport(config);
 
 const sendEmail = async (data) => {
-  const emailOptions = { ...data, from: META_EMAIL };
+  const emailOptions = { ...data, from: USER_EMAIL };
   await transporter.sendMail(emailOptions);
   return true;
 };
